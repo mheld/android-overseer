5cf70feb760c230bbd500d332d8dc2cb56a64681
mheld <heldgeek@gmail.com>
2011-11-16 23:10:16 -0500

    woah, forgot to add the maps library

--- src/com/overseer/Viewer.java
+++ src/com/overseer/Viewer.java
@@ -1,13 +1,44 @@
 package com.overseer;
 
-import android.app.Activity;
+import java.util.List;
+
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView;
+import com.google.android.maps.Overlay;
+import com.google.android.maps.OverlayItem;
+import com.overseer.utils.BasicItemizedOverlay;
+
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
-public class Viewer extends Activity {
-    /** Called when the activity is first created. */
+public class Viewer extends MapActivity {
+	MapView mMapView;
+	
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        mMapView = (MapView)findViewById(R.id.main_map_view);
+        
+        List<Overlay> mapOverlays = mMapView.getOverlays();
+        Drawable drawable = this.getResources().getDrawable(R.drawable.androidmarker);
+        BasicItemizedOverlay itemizedoverlay = new BasicItemizedOverlay(drawable);
+        
+        GeoPoint point = new GeoPoint(19240000,-99120000);
+        OverlayItem overlayitem = new OverlayItem(point, "Hola, Mundo!", "I'm in Mexico City!");
+        
+        itemizedoverlay.addOverlay(overlayitem);
+        mapOverlays.add(itemizedoverlay);
+        
     }
+
+
+	@Override
+	protected boolean isRouteDisplayed() {
+		// TODO Auto-generated method stub
+		return false;
+	}
 }
--- src/com/overseer/utils/Prefs.java
+++ src/com/overseer/utils/Prefs.java
@@ -2,5 +2,6 @@ package com.overseer.utils;
 
 public class Prefs {
 	public static final String PREFS_NAME = "PrefsFile";
+	public static final boolean debug = true;
 
 }
